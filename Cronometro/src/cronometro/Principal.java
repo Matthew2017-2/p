623a27cf63e4a98c0ee1c9c81a15ffa7e0e13182
Matthew2017-2 <izquierdo.mateo11@gmail.com>
2018-02-26 08:03:19 -0500

    inicio repo

--- Cronometro/src/cronometro/Principal.java
+++ Cronometro/src/cronometro/Principal.java
@@ -6,6 +6,9 @@
 package cronometro;
 
 import cronometro.logica.Cronometro;
+import cronometro.logica.UnidadTiempo;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
@@ -18,21 +21,49 @@ public class Principal {
      */
     public static void main(String[] args) {
         Cronometro c = new Cronometro();
-        
-        for (int i = 0; i < 1000; i++) {
-            c.avanzar();
-            System.out.println(c.obtenerTiempo());
-            if(i % 100 == 0){
-                c.guardarMemoria();
+        int x=0;
+        Timer timer;
+        timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                 if(x==0){
+                 if(!c.obtenerTiempo().equals("00 : 02 : 00 : 0")) {
+                    c.avanzar();
+                    System.out.println(c.obtenerTiempo());
+                   
+                 }else{
+                  x++;
+                    
+                }
+                }
+                 
+               
             }
-        }
-        System.out.println("Memorias");
-        c.mostrarMemorias();
-        System.out.println("Retrocediendo");
-        for(int i = 0; i < 1000; i++){
-            c.retroceder();
-            System.out.println(c.obtenerTiempo());
-        }
-    }
-    
+        };
+        // Empezamos dentro de 0ms y luego lanzamos la tarea cada 100ms
+
+        timer.schedule(task, 0, 1);
+
+//        for (int i = 0; i < 1000; i++) {
+//
+//            
+//            timer.schedule(task, 0, 100);
+//            
+//            if (i % 100 == 0) {
+//                c.guardarMemoria();
+//            }
+//        }
+//        System.out.println("Memorias");
+//        c.mostrarMemorias();
+//        System.out.println("Retrocediendo");
+//       
+//         for (int i = 0; i < 1000; i++) {
+
+//            c.retroceder();
+//            System.out.println(c.obtenerTiempo());
+//        }
+   }
+
 }
